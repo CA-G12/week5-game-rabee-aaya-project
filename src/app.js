b6a25ddbf18056aebf23f5a35986abc71007173f
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-08-17 12:25:45 +0300

    get data from api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-const path = require('path');
+// const path = require('path');
 const express = require('express');
+const router = require('./routers');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(router);
 
 module.exports = app;
