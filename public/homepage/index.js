b4e41c3601141828e8f06c6d390793a0a51af0cf
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-08-17 15:26:59 +0300

    create homepage structure related to #15

--- public/homepage/index.js
+++ public/homepage/index.js
@@ -1,1 +1,4 @@
 fetch('/data').then((response) => response.json()).then((data) => console.log(data.results[0].name));
+
+fetch('/data').then((response) => response.json()).then((data) => handelData(data));
+//https://api.rawg.io/api/games?search=call&key=e735fb7f64764085aed20235ec7455f9
