6999165bf30aa844267109f8307022ac52fdd856
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-08-18 09:56:10 +0300

    edits

--- src/app.js
+++ src/app.js
@@ -11,4 +11,13 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(router);
 
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '..', 'public', '404.html'));
+  });
+  
+  app.use((err, req, res, next) => {
+    res.status(500).sendFile(path.join(__dirname, '..', 'public', '500.html'));
+  });
+
 module.exports = app;
